7c9fe910a41d6ead263a3468db1e5aa67c2516cd
hectorsvill <hectorstevenvillasano@gmail.com>
2019-12-17 11:02:10 -0800

    import SwiftSoup

--- Package.swift
+++ Package.swift
@@ -5,18 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "QuotesDictionaryCreator",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    platforms: [.macOS(.v10_13)],
+    dependencies: [.package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.2.2")],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "QuotesDictionaryCreator",
-            dependencies: []),
-        .testTarget(
-            name: "QuotesDictionaryCreatorTests",
-            dependencies: ["QuotesDictionaryCreator"]),
+        .target(name: "QuotesDictionaryCreator", dependencies: ["SwiftSoup"]),
+        .testTarget(name: "QuotesDictionaryCreatorTests", dependencies: ["QuotesDictionaryCreator"]),
     ]
 )
--- Sources/QuotesDictionaryCreator/main.swift
+++ Sources/QuotesDictionaryCreator/main.swift
@@ -1,1 +1,4 @@
-print("Hello, world!")
+import Foundation
+import SwiftSoup
+
+
