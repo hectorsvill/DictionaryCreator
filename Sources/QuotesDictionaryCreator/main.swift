33df568e3e8144a1437a63dce63e568cff8725dc
hectorsvill <hectorstevenvillasano@gmail.com>
2020-04-23 10:49:01 -0700

    Created a new class called PokemonBucket to download a list of pokemon images from url.

--- Sources/QuotesDictionaryCreator/main.swift
+++ Sources/QuotesDictionaryCreator/main.swift
@@ -1,4 +1,4 @@
-import Foundation
+import Cocoa
 import SwiftSoup
 
 open class QuotesDictionaryCreator {
@@ -6,11 +6,11 @@ open class QuotesDictionaryCreator {
     let url = URL(string: "https://wisdomquotes.com/")!
     
     init () {
-        createQuoteDict()
+        createQuotesDictionary()
     }
     
     /// Create a dictionary of quotes
-    open func createQuoteDict() {
+    open func createQuotesDictionary() {
          let quoteLinks = getAllQuoteLinks()
          QuotesDictionary["results"] = []
         
@@ -18,7 +18,6 @@ open class QuotesDictionaryCreator {
              do {
                 let data = try Data(contentsOf: v)
                 let html = String(data: data, encoding: .utf8)!
-                
                 scrapeQuotes(html: html, type: k)
              } catch {
                  NSLog("Error: \(error)")
@@ -107,4 +106,64 @@ open class QuotesDictionaryCreator {
 }
 
 
+open class PokemonBucket {
+    let baseURLString = "https://www.serebii.net/swordshield/pokemon/"
+    let bucketName: String
 
+    init(bucketName: String) {
+        self.bucketName = bucketName
+    }
+
+    open func fetchPokemonImages(with folderName: String) {
+        for index in 1...890 {
+            let indexString = createPokemonIndex(index: index)
+            let urlString = "\(baseURLString)\(indexString).png"
+            let url = URL(string: urlString)!
+
+            do {
+                let data = try Data(contentsOf: url)
+                saveDataToDesktopFolder(data, imageName: indexString, folderName: folderName)
+            } catch {
+                print(error)
+            }
+        }
+
+    }
+
+    private func saveDataToDesktopFolder(_ data: Data, imageName: String, folderName: String) {
+        let fileManager = FileManager()
+        var directoryURL = try! fileManager.url(for: .desktopDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        directoryURL.appendPathComponent(bucketName)
+
+        try! fileManager.createDirectory(at: directoryURL, withIntermediateDirectories: true, attributes: nil)
+
+        directoryURL.appendPathComponent(folderName)
+        try! fileManager.createDirectory(at: directoryURL, withIntermediateDirectories: true, attributes: nil)
+
+        directoryURL.appendPathComponent("\(imageName).png")
+        fileManager.createFile(atPath: directoryURL.path, contents: data, attributes: nil)
+
+    }
+
+    private func createPokemonIndex(index: Int) -> String {
+        var indexLength = 0;
+        var indexCopy = index;
+        
+        while (indexCopy > 0) {
+            indexLength += 1;
+            indexCopy /= 10;
+        }
+        
+        var zerosString = ""
+
+        while (indexLength < 3) {
+            zerosString += "0"
+            indexLength += 1;
+        }
+        
+        return  "\(zerosString)\(index)"
+    }
+}
+
+//var imagesBucket = PokemonBucket(bucketName: "Pokemon_Bucket2")
+//imagesBucket.fetchPokemonImages(with: "Sword Shield Images")
